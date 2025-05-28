370a709ca7d833128c86bff5f11c4ffbe8580f12
Asep-Jelpa-Nasution <jelpanasution@gmail.com>
2025-05-29 04:11:11 +0700

    Initial commit

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,16 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\Debug\Toolbar\Collectors\Views;
+
 class Home extends BaseController
 {
     public function index(): string
     {
         return view('welcome_message');
+    }
+    public function tambah()
+    {
+        return view('view/form_add'); // sesuaikan nama file view kamu
     }
 }
